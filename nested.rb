01cf2db1a2f1c3751aaa6d69ec85b1cdab67e2cd
Jordan Carroll <jordancarroll1994@gmail.com>
2020-11-11 04:07:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,3 @@
-
 def hopper
 	programmer_hash = 
  		{
@@ -16,6 +15,7 @@ def hopper
       }
     }
 
+programmer_hash[:grace_hoper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,8 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+    
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +56,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+    
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -81,7 +83,9 @@ def adding_matz
         :languages => ["C"]
       }
     }
+programmer_hash[:yukihiro_matsumoto]=>{:known_for => "Ruby", :languages => ["LISP","C"]}]
 
+puts programmer_hash
 end
 
 def changing_alan
@@ -103,7 +107,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
+programmer_hash[:alan_kay][:known_for] = "GUI"
 
+puts programmer_hash
 end
 
 def adding_to_dennis
@@ -126,4 +132,6 @@ def adding_to_dennis
       }
     }
 
+programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
+puts programmer_hash
 end
